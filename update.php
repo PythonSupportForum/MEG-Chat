15dede27308b8ee9e19ef071672226870f4d1716
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-05 08:59:32 +0100

    1

--- update.php
+++ update.php
@@ -1,12 +1,12 @@
 <?
 $out = array();
 $status = -1;
-$message=exec("sudo -u root -S bash /var/www/html/MEG-Chat/update.sh < /home/tilo2/password.txt", $output, $return_var);
+$message=exec("bash /var/www/html/MEG-Chat/update.sh < /home/tilo2/password.txt", $output, $return_var);
 echo implode(", ", $output);
 echo "<br>";
-echo $return_var;
+echo "<strong>Status Code: </strong>".$return_var;
 echo "<br>";
-echo $message;
+echo "<strong>Rückgabe: </strong><br>".$message;
 
 if ( $status == 0 ) {
     ?>
