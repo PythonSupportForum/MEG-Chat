7ee9a21ca603463d4e38365fee6fa4430e38f31d
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-05 08:19:08 +0100

    2

--- update.php
+++ update.php
@@ -1,5 +1,10 @@
 <?
-$message=shell_exec("sudo -u root -S bash /var/www/html/MEG-Chat/update.sh < /home/tilo2/password.txt");
+$message=exec("sudo -u root -S bash /var/www/html/MEG-Chat/update.sh < /home/tilo2/password.txt", $output, $return_var);
+echo $outpu;
+echo "<br>";
+echo $return_var;
+echo "<br>";
 echo $message;
+
 ?>
 <p style="color: green; ">Erfolgreich gespeichert!</p>
