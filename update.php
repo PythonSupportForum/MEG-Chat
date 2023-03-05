38fd44e0890f4ecebcb799118fb0a25935ff17a1
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-05 08:23:55 +0100

    git

--- update.php
+++ update.php
@@ -1,4 +1,6 @@
 <?
+$out = array();
+$status = -1;
 $message=exec("sudo -u root -S bash /var/www/html/MEG-Chat/update.sh < /home/tilo2/password.txt", $output, $return_var);
 echo implode(", ", $output);
 echo "<br>";
@@ -6,6 +8,13 @@ echo $return_var;
 echo "<br>";
 echo $message;
 
+if ( $status == 0 ) {
+    ?>
+    <p style="color: green; ">Erfolgreich gespeichert!</p>
+    <?
+} else {
+	?>
+    <p style="color: red; ">Fehler beim Speichern!</p>
+    <?
+}
 ?>
-<h1>OLEOLEOLE</h1>
-<p style="color: green; ">Erfolgreich gespeichert!</p>
