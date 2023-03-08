f95d7e2e795bb0a6b973219db752c261e7d57feb
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-08 19:52:33 +0100

    test

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,6 +19,20 @@ window.addLoadEvent = function(func) {
     }
   }
 }
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
 addLoadEvent(function(){
 	runned_onload = true;
     if ("serviceWorker" in navigator) {
