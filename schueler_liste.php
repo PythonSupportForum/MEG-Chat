5163a620c537b170333b8cfaf6de9ad584a1ed09
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-08 21:42:33 +0100

    schüler lsite

--- schueler_liste.php
+++ schueler_liste.php
@@ -76,7 +76,30 @@ if(isset($_SESSION['pupil'])){
         <div style="float: left; width: calc( 100% - 162px ); min-width: 600px; max-width: 100%; text-align: center;">
             <h1>Suchen Sie nach Schülern auf dem MEG-Chat</h1>
 			<div style="width: 100%; height: 100px; " class="centriert"><input type="text" placeholder="Suchen.." id="search" style="width: 500px; max-width: 90%; height: 50px; font-size: 24px; "></div>
-			<div style="width: 100%; "></div>
+			<div style="width: 100%; ">
+			    <?
+				$stmtData = $db->prepare("SELECT * FROM ".DBTBL.".pupils WHERE activated = 1 ORDER BY fullname ASC LIMIT 10000;");
+				$stmtData->execute();
+				while($row = $stmtData->fetchObject()){ $row = (array)$row; ?>
+					<a href="javascript:page_navigate('/schueler/<? echo htmlspecialchars($row['id']); ?>');" style="color: black; text-decoration: none; "><div class="schueler_container">
+					    <div style="height: calc( 100% - 60px ); width: 100%; margin-top: 10px; " class="centriert">
+					        <img loading="lazy" style="width: 130px; height: 130px; border-radius: 50%; " src="<? echo htmlspecialchars(empty($row['avatar']) ? "/resources/images/avatar.png" : $row['avatar']); ?>">
+					    </div>
+					    <div style="width: 100%; height: 40px; word-wrap: break-word; " class="centriert">
+					        <h3 style="word-wrap: break-word;"><? echo htmlspecialchars($row['fullname']); ?></h3>
+					    </div>
+					    <div style="width: 100%; height: 20px; font-size: 14px; " class="centriert">
+					        <div style="width: 100%; text-align: center; word-wrap: break-word; " class="centriert">
+								<div style="text-align: center; width: auto; height: auto; ">
+									<div class="schueler_vote_count_<? echo htmlspecialchars($row['id']); ?>" style="float: left; color: black; "><? echo $row['rating']; ?></div>
+									<div style="float: left; margin-left: 8px; color: black; "><? echo htmlspecialchars($row['about_me']); ?></div>
+								</div>
+							</div>
+					    </div>
+						<div style="width: 100%; height: 10px; "></div>
+					</div></a>
+				<? } ?>
+			</div>
         </div>
     </body>
 </html>
