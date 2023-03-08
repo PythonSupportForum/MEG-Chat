c13011b127cfad4a41939693307e47d52c0a12f9
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-08 19:43:00 +0100

    hi

--- push_register.php
+++ push_register.php
@@ -1,28 +1,12 @@
 <?
-use MinishlinkWebPushWebPush;
 require_once("db.php");
 
 echo json_encode($_POST);
 
-
-
 $publicKey = "BGW50vjoGBEKXC6onSyjfwZFF5tuLzQEyd4Bu-H5tFm9tp5AmleyjuC1Aec-RzJSCnUHA60aaDWoZkNBbjQHGF8";
 $privateKey = "KMeD4lvYxi5VYYS4qwzQxWh-jJXiH_G-Qpr3y_cqK1U";
 
-function getMyUrl(){
-  $protocol = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')) ? 'https://' : 'http://';
-  $server = $_SERVER['SERVER_NAME'];
-  $port = $_SERVER['SERVER_PORT'] ? ':'.$_SERVER['SERVER_PORT'] : '';
-  return $protocol.$server.$port;
-}
 
-$webPush = new WebPush([
-    "VAPID" => [
-        "subject" => getMyUrl(),
-        "publicKey" => $publicKey,
-        "privateKey" => $privateKey
-    ]
-]);
 
 
 ?>
