75c1dbc6da23535b0a74910e40e75fd4d53f3ae3
AureumApes <janundartur@gmail.com>
2023-03-09 20:47:07 +0100

    Added Contributers to Imprint

--- internal/information/impressum.php
+++ internal/information/impressum.php
@@ -6,8 +6,12 @@
     <link rel="stylesheet" href="/resources/css/tailwind.css">
 </head>
 <body class="bg-slate-600 text-center text-white">
-    <h1>Impressum</h1>
-    <p>Tilo Behnke<br>Berliner Ring 51<br>50321, Brühl<br><br><b>Tel: </b>+49 015752250315<br><b>Email: </b>behnke.tilo@meg-bruehl.de</p>
-    <a href="/">Zurück zur Startseite</a>
+<h1>Impressum</h1>
+<p>Tilo Ulf Behnke<br>Berliner Ring 51<br>50321, Brühl<br><br><b>Tel: </b>+49 015752250315<br><b>Email: </b>behnke.tilo@meg-bruehl.de
+</p>
+<a href="/">Zurück zur Startseite</a>
+<h2>Mitwirkende</h2>
+<li>Artur Darius Herrmann</li>
+<li>Florian Reschke</li>
 </body>
 </html>
