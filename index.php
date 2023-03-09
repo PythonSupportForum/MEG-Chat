d0e4590d3262d5d1846d9f5dd9858c18e922fce3
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-09 17:11:50 +0100

    Startseite Zentriert

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if(isset($_SESSION['pupil'])){
 	        </div>
         </div>
         <div>
-			<div style="width: 100%; margin-top: 20px; ">
+			<div style="width: 100%; margin-top: 20px; " class="centriert">
 		        <div id="all_container">
                     <?php require("internal/information/public_chats.php"); ?>
 				</div>
