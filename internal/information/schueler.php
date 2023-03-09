df6adde07734467e5e810ddbaeb7e7394f970e70
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-09 17:09:50 +0100

    Design Von Schüler geändert

--- internal/information/schueler.php
+++ internal/information/schueler.php
@@ -100,15 +100,15 @@ $s_data = (array)$row;
 						  <div style="width: 100%; height: 20px; font-size: 14px; ">
 			                  <div style="width: 100%; text-align: left ">
 							      <div style="text-align: left; width: auto; height: auto; ">
-									  <? if(isset($_SESSION['pupil']) && $pupil_data['activated'] == 1 && $s_data['id'] != $pupil_data['id']){ ?><a style="color: black; " href="javascript:void(0); " onclick="event.stopPropagation(); vote('<? echo htmlspecialchars($s_data['id']); ?>');"><? } ?>
-									  <div class="schueler_vote_count_<? echo htmlspecialchars($s_data['id']); ?>" style="float: left; color: black; "><? echo $s_data['rating']; ?></div>
-									  <div style="float: left; margin-left: 8px; color: black; ">Stimmen</div>
+									  <? if(isset($_SESSION['pupil']) && $pupil_data['activated'] == 1 && $s_data['id'] != $pupil_data['id']){ ?><a style="color: white; " href="javascript:void(0); " onclick="event.stopPropagation(); vote('<? echo htmlspecialchars($s_data['id']); ?>');"><? } ?>
+									  <div class="schueler_vote_count_<? echo htmlspecialchars($s_data['id']); ?>" style="float: left; color: white; "><? echo $s_data['rating']; ?></div>
+									  <div style="float: left; margin-left: 8px; color: white; ">Stimmen</div>
 									  <? if(isset($_SESSION['pupil']) && $pupil_data['activated'] == 1 && $s_data['id'] != $pupil_data['id']){ ?></a><? } ?>
 								  </div>
 							  </div>
 						  </div>
 						  <? if(isset($_SESSION['pupil']) && $pupil_data['activated'] == 1 && $s_data['id'] != $pupil_data['id']){ ?>
-					      <div style="width: 100%; height: 25px; font-size: 14px; ">
+					      <div style="width: 100%; height: 25px; font-size: 14px; margin-top: 15px; ">
 							  <button onclick="event.stopPropagation(); vote('<? echo htmlspecialchars($s_data['id']); ?>');">Gefällt Mir</button>
 						  </div>
 						  <? }?>
