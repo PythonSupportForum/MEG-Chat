1aaa931363d4973cbf5be9cb555334010698514b
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-11 09:32:17 +0100

    test

--- internal/information/chat.php
+++ internal/information/chat.php
@@ -53,7 +53,11 @@ if($chat_data){
 	$messages_count = ((array)$stmtMessagesCount->fetchObject())['count'];
 }
 
-$is_mobile = preg_match("/(android|webos|avantgo|iphone|ipad|ipod|blackberry|iemobile|bolt|boost|cricket|docomo|fone|hiptop|mini|opera mini|kitkat|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+if(isset($_SERVER['HTTP_USER_AGENT'])){
+    $is_mobile = preg_match("/(android|webos|avantgo|iphone|ipad|ipod|blackberry|iemobile|bolt|boost|cricket|docomo|fone|hiptop|mini|opera mini|kitkat|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+} else {
+	$is_mobile = true;
+}
 ?>
 <!DOCTYPE html>
 <html lang="de">
