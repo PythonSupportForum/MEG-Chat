6bb0917a4d53e5e3bbeff009064395be98a16c8d
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-03-13 18:01:34 +0100

    Update blog.php

--- internal/information/blog.php
+++ internal/information/blog.php
@@ -68,7 +68,7 @@ if(isset($_SERVER['HTTP_USER_AGENT'])){
         <?php } else { ?>
 	    <button onclick="window.history.go(-1); " style="position: fixed; top: 0px; left: 0px; min-height: 50px; height: auto; width:  auto; font-size: 16px; background-color: transparent; font-size: 24px; color: white; border: none; outline: none; ">&#8678;</button>
 		<?php } ?>
-        <div style="float: left; width <?php if(!$is_mobile){ ?>calc( 100% - 162px )<?php } else { ?>100%<?php } ?>; min-width: 300px; max-width: 100%; text-align: center;" class="centriert">
+        <div style="float: left; width: <?php if(!$is_mobile){ ?>calc( 100% - 162px )<?php } else { ?>100%<?php } ?>; min-width: 300px; max-width: 100%; text-align: center;" class="centriert">
             <div style="height: 100%; ">
             <?php if(!$blog_data){
 			    ?>
